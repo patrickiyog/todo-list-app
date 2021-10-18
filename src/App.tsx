41e64d3398a711a55333e5caa770f07173d43a17
patrickiyog <patrickiyog97@gmail.com>
2021-10-18 19:52:02 +1300

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { TaskContext } from './context/TaskContext';
 import './App.css';
 import { Task } from './interfaces/Task';
-import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
 
 const App: React.FC = () => {
