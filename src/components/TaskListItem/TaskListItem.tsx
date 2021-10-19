36adc3b655c37ca936384ea93437ed735514eadf
patrickiyog <patrickiyog97@gmail.com>
2021-10-19 19:53:30 +1300

    create task lists

---
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TaskListItem.css';
+
+const TaskListItem = () => {
+
+    return (
+        <div className="task-list-item">
+            <div>
+                Task List Item
+            </div>
+        </div>
+    );
+
+}
+
+export default TaskListItem;
--- src/components/TaskLists/TaskLists.tsx
+++ src/components/TaskLists/TaskLists.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './TaskLists.css';
 import Logo from '../Logo/Logo';
+import TaskListItem from '../TaskListItem/TaskListItem';
 
 const TaskLists = () => {
 
     return (
         <div className="task-lists">
-            <Logo />
+            <div className="logo-container"> 
+                <Logo />
+            </div>
+            <div className="task-list-item-container">
+                <TaskListItem />
+            </div>
         </div>
     );
 
