ec2d9015de6ef31b5061323a79caa43f1aa53814
patrickiyog <patrickiyog97@gmail.com>
2021-11-09 20:35:30 +1300

    select labels radio

--- src/components/TaskLabelForm/TaskLabelForm.tsx
+++ src/components/TaskLabelForm/TaskLabelForm.tsx
@@ -1,29 +1,64 @@
-import React, { useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 import TaskLabel from '../TaskLabel/TaskLabel';
 import './TaskLabelForm.css';
 import { MdRadioButtonUnchecked, MdRadioButtonChecked } from "react-icons/md";
 
+interface Labels {
+    [label: string]: boolean;
+}
+
 const TaskLabelForm = () => {
-    const [selectedLabel, setSelectedLabel] = useState('');
+
+    const [labels, setLabels] = useState<Labels>({
+        high: false,
+        medium: false,
+        low: false
+    });
+
+    const handleClick = (event: MouseEvent<SVGAElement>) => {
+        const clickedLabel = event.currentTarget.id;
+        const newLabels = { ...labels };
+        for (const label in newLabels) {
+            if (label === clickedLabel) {
+                newLabels[clickedLabel] = !newLabels[clickedLabel];
+            } else {
+                newLabels[label] = false;
+            }
+        }
+        setLabels(newLabels);
+    }
+
     return (
         <div>
             <div className="label-title">Labels</div>
             <div className="task-labels">
                 <div className="task-label-container">
-                    <div className="task-label-radio">
-                        <MdRadioButtonUnchecked />
+                    <div className="task-label-radio" >
+                        {
+                            labels.high
+                                ? <MdRadioButtonChecked id="high" onClick={handleClick} />
+                                : <MdRadioButtonUnchecked id="high" onClick={handleClick} />
+                        }
                     </div>
                     <TaskLabel labelName={"High Priority"} type={"HP"} />
                 </div>
                 <div className="task-label-container">
                     <div className="task-label-radio">
-                        <MdRadioButtonUnchecked />
+                        {
+                            labels.medium
+                                ? <MdRadioButtonChecked id="medium" onClick={handleClick} />
+                                : <MdRadioButtonUnchecked id="medium" onClick={handleClick} />
+                        }
                     </div>
                     <TaskLabel labelName={"Medium Priority"} type={"MP"} />
                 </div>
                 <div className="task-label-container">
-                    <div className="task-label-radio">
-                        <MdRadioButtonUnchecked />
+                    <div id="low" className="task-label-radio">
+                        {
+                            labels.low
+                                ? <MdRadioButtonChecked id="low" onClick={handleClick} />
+                                : <MdRadioButtonUnchecked id="low" onClick={handleClick} />
+                        }
                     </div>
                     <TaskLabel labelName={"Low Priority"} type={"LP"} />
                 </div>
@@ -31,6 +66,7 @@ const TaskLabelForm = () => {
 
         </div>
     );
+
 }
 
 export default TaskLabelForm;
